8088a0886b0d48d801adaec737da0575533619ef
teeengmod26 <lightertmz@outlook.com>
2023-05-21 13:07:53 +0800

    Add files via upload

---
+++ ex1.1.py
@@ -0,0 +1,11 @@
+# ex1.1.py
+# 让你的脚本多打印一行
+print("Hello World!")
+print("Hello Again")
+print("I like typing this")
+print("This is fun.")
+print("Yay! printing.")
+print("I'd much rather you 'not.")
+print('I "Said" do not touch this.')
+print("HAHA")
+
