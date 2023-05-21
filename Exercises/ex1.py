3463c1f6899ba7c264b239f9b3a78f1c21303344
teeengmod26 <lightertmz@outlook.com>
2023-05-21 14:04:29 +0800

    Create ex1.py

---
+++ Exercises/ex1.py
@@ -0,0 +1,9 @@
+# 第一个程序
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this")
+print("This is fun.")
+print("Yay! printing.")
+print("I'd much rather you 'not.")
+print('I "Said" do not touch this.')
