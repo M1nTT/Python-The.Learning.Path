70175598668d8674108c15932c27e1f1e60ab8a9
teeengmod26 <lightertmz@outlook.com>
2023-05-21 14:04:48 +0800

    Add files via upload

---
+++ Exercises/ex3.1.py
@@ -0,0 +1,35 @@
+# ex3.1.py
+# 每一行的上面使用#为自己写一个注释，说明一下这行的作用。
+
+# 打印出字符串
+print("I will now count my chickens:")  
+
+# 打印出字符串，再输出算式结果
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)  
+
+# 打印出字符串
+print("Now I will count the eggs:") 
+# 输出算式结果
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)  
+
+# 打印出字符串
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# 判断不等式是否成立，成立：True，不成立：Flase
+print(3 + 2 < 5 -7)
+
+# 打印出字符串，再输出算式结果
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7", 5 - 7)
+
+# 打印出字符串
+print("Oh, that's why it's False.")
+
+# 打印出字符串
+print("How about some more.")
+
+# 打印出字符串，再判断不等式是否成立，成立：True，不成立：Flase
+print("Is it greater?", 5 < -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ Exercises/ex3.2.py
@@ -0,0 +1,26 @@
+# ex3.2.py
+# 使用浮点数重写一遍 ex3.py，让它的计算结果更准确。提示：20.0 就是一个浮点数。
+
+print("I will now count my chickens:")
+
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("Now I will count the eggs:")
+
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 < -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
